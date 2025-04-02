f3e3e0c9cbadd746cb5b423c2e08924da228ab3c
Pavel Kholkin <xpaul6@gmail.com>
2025-04-02 19:15:42 +0500

    Initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  preview: {
+    port: 80,
+    strictPort: true,
+    host: true
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
