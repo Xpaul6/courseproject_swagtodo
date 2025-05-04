bbc7676616375401a37d5292d5056a8f5c479a18
Pavel Kholkin <xpaul6@gmail.com>
2025-05-04 18:22:34 +0500

    feat: index.html correct title & icon

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [apiText, setApiText] = useState('');
+
+  // async function getApiText() {
+  //   await fetch('/api/hello')
+  //     .then((res) => res.json())
+  //     .then((data) => {setApiText(data.message)})
+  //     .catch((err) => 'Error: ' + err.message)
+  // }
+
+  useEffect(async () => {setApiText('Hello!')}, [])
+
+  return (
+    <>
+      <div className='hello'>{apiText}</div>
+    </>
+  )
+}
+
+export default App
