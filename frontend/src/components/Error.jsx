be243ab22da877f38494cf678643ad8f0e4c6ffe
Pavel Kholkin <xpaul6@gmail.com>
2025-05-04 17:55:53 +0500

    feat: specified error handling

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 
-function Error() {
+function Error({type}) {
   return (
     <div>
       <h2>An error occured</h2>
+      <p>Type: {type}</p>
       <Link to='/'>Reload</Link>
     </div>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Error />, 
+    errorElement: <Error type="incorrect page url"/>, 
   },
 ])
 
