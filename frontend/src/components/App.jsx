894e063dc549ce9a7bfeb04f936c8e2b7007fc87
Pavel Kholkin <xpaul6@gmail.com>
2025-05-08 19:30:36 +0500

    fix: "/" page common styling added

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,43 +1,33 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+import '../styles/main.css'
+
 function App() {
-  const [apiText, setApiText] = useState('defaultText');
+  const [apiText, setApiText] = useState('defaultApiText');
 
   // async function getApiText() {
   //   await fetch('/api/hello')
   //     .then((res) => res.json())
   //     .then((data) => {setApiText(data.message)})
-  //     .catch((err) => 'Error: ' + err.message)
+  //     .catch((err) => console.log('Error: ' + err.message))
   // }
 
   // useEffect(getApiText, [])
 
   return (
     <>
-      <div className="flex flex-col items-center w-full p-6 mt-50 sm:border-2 border-blue-400 rounded-xl">
+      <div className="flex flex-col items-center w-full py-6 px-10 mt-50 sm:border-2 border-blue-400 rounded-xl">
         <h2>SWAG ToDo</h2>
         <p>{apiText}</p>
         <div className="flex flex-col items-center mt-4">
-          <Link
-            to="/a"
-            className="text-xl text-blue-600 text-center w-full py-4 px-6 my-2 rounded-xl hover:bg-gray-100
-              hover:cursor-pointer hover:underline transition ease duration-150"
-          >
+          <Link to="/a" className="link">
             Регистрация
           </Link>
-          <Link
-            to="/a"
-            className="text-xl text-blue-600 text-center w-full py-4 px-6 my-2 rounded-xl hover:bg-gray-100
-              hover:cursor-pointer hover:underline transition ease duration-150"
-          >
+          <Link to="/a" className="link">
             Войти как родитель
           </Link>
-          <Link
-            to="/a"
-            className="text-xl text-blue-600 text-center w-full py-4 px-6 my-2 rounded-xl hover:bg-gray-100
-              hover:cursor-pointer hover:underline transition ease duration-150"
-          >
+          <Link to="/a" className="link">
             Войти как ребенок
           </Link>
         </div>
