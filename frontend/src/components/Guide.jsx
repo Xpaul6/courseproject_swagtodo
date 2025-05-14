8baf76889de864816fc78db9d43926705b6f414c
Pavel Kholkin <xpaul6@gmail.com>
2025-05-14 19:08:12 +0500

    please work

--- frontend/src/components/Guide.jsx
+++ frontend/src/components/Guide.jsx
@@ -1,20 +1,4 @@
-import { useContext } from "react"
-import { GuideContext } from "../context/GuideContext"
-
-function DefineLocation(path) {
-  console.log(path)
-  try {
-    if (path === "/") return "/guidePage/glavnoe_menyu_sajta.htm"
-  } catch(e) {
-    console.log(e)
-  } finally {
-    return "/guidePage/glavnoye_menyu_saita.htm"
-  }
-}
-
 function Guide() {
-  const { path, setPath } = useContext(GuideContext)
-
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <iframe
