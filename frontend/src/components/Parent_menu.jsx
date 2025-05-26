aea41a1783da9f5b3d4d2e5a4e27ef2773f52d2e
Pavel Kholkin <xpaul6@gmail.com>
2025-05-26 14:48:12 +0500

    feat: child stats blovk layout

--- frontend/src/components/Parent_menu.jsx
+++ frontend/src/components/Parent_menu.jsx
@@ -1,18 +1,20 @@
 import '../styles/main.css'
+
 import Task from './blocks/Task'
+import Child from './blocks/Child';
 
 function Parent_menu() {
   return (
     <>
       <div className="p-5 w-full">
         <h2 className="text-center">Меню родителя</h2>
-        <div className="flex sm:flex-row sm:justify-around flex-col mt-10 sm:h-[400px] sm:ml-6">
-          <div className="overflow-x-auto whitespace-nowrap sm:overflow:visible sm:whitespace-normal">
+        <div className="flex sm:flex-row sm:justify-around flex-col mt-10 sm:h-[400px]">
+          <div className="overflow-x-auto whitespace-nowrap sm:overflow-visible sm:whitespace-normal">
             <div
-              className="flex flex-row sm:flex-col justify-evenly min-w-full border-b-2 pb-1 sm:pb-0 sm:border-2 sm:h-full
+              className="flex flex-row sm:flex-col justify-evenly sm:min-w-[100px] border-b-2 pb-1 sm:pb-0 sm:border-2 sm:h-full
               border-gray-300 sm:rounded-md"
             >
-              <div className="underline text-blue-600 mx-1.5 text-center flex align-center leading-8 sm:hover:cursor-pointer">
+              <div className="underline text-blue-600 mx-1.5 text-center leading-8 sm:hover:cursor-pointer">
                 Список 1
               </div>
               <div className="mx-1.5 text-center leading-8 sm:hover:cursor-pointer">
@@ -40,14 +42,13 @@ function Parent_menu() {
               +
             </button>
           </div>
-          <div className="relative mt-10 sm:mt-0 flex flex-col p-2 pt-0 sm:w-2/6 border-2 border-indigo-400 rounded-md sm:ml-15 sm:mr-6">
+          <div className="relative mt-10 sm:mt-0 flex flex-col p-2 pt-0 sm:w-2/6 border-2 border-indigo-400 rounded-md sm:ml-15 sm:mr-6 overflow-x-scroll">
             <h3 className="sticky text-center mb-2 pt-2 pb-2 border-b-1 border-indigo-200">
               Статистика
             </h3>
-            <div className="flex justify-evenly">
-              <div className="">Имя Фамилия</div>
-              <div>2/10</div>
-            </div>
+            <Child />
+            <Child />
+            <Child />
             <button className="sticky bottom-1 right-1 ml-auto mt-auto py-2 px-3.5 w-min text-xl text-white border-2
               rounded-xl bg-green-500 sm:cursor-pointer">
               +
---
+++ frontend/src/components/blocks/Child.jsx
@@ -0,0 +1,12 @@
+function Child() {
+  return (
+    <>
+      <div className="flex justify-evenly my-2">
+        <div className="">Имя Фамилия</div>
+        <div>2/10</div>
+      </div>
+    </>
+  );
+}
+
+export default Child
