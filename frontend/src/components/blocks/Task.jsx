628626cf3c2e8dc71d1e93384d37947ac5f99d41
Pavel Kholkin <xpaul6@gmail.com>
2025-05-26 14:47:58 +0500

    feat: task block layout

--- frontend/src/components/blocks/Task.jsx
+++ frontend/src/components/blocks/Task.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react'
+
 function Task() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div className="flex justify-around items-center my-2">
-      <div className="w-fit">Task description</div>
-      <button className="absolute right-7">...</button>
+    <div className="flex justify-around items-center my-2 relative">
+      <div className={`w-fit mx-10 ${isOpen ? "underline" : ""}`}>Task description</div>
+      <button
+        className="absolute right-3 p-2 rotate-90 cursor-pointer"
+        onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
+      >
+        {/* {isOpen? '▲' : '▼'} */}
+        {isOpen ? '<' : '>'}
+      </button>
+      <div
+        className={`absolute right-7 bg-white top-8 flex flex-col justify-center shadow-md rounded-md
+          overflow-hidden transition-all duration-200 ease-in-out ${isOpen ? "max-h-36 z-10" : "max-h-0 z-10"}`}
+      >
+        <button className="bg-white hover:bg-gray-100 px-4 py-2 text-left cursor-pointer">
+          Редактировать
+        </button>
+        <button className="bg-white hover:bg-gray-100 px-4 py-2 text-left cursor-pointer">
+          Удалить
+        </button>
+        <button className="bg-white hover:bg-gray-100 px-4 py-2 text-left cursor-pointer">
+          Выполнить
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Task
