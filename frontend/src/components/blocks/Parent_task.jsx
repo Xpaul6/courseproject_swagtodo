9c7fc9933bca841a90dfb634743ba9e621fa8515
Pavel Kholkin <xpaul6@gmail.com>
2025-05-28 18:26:32 +0500

    feat: new popup window button icon

--- frontend/src/components/blocks/Parent_task.jsx
+++ frontend/src/components/blocks/Parent_task.jsx
@@ -7,15 +7,14 @@ function Parent_task() {
     <div className="flex justify-around items-center my-2 relative">
       <div className={`w-fit mx-10 ${isOpen ? "underline" : ""}`}>Task description</div>
       <button
-        className="absolute right-3 p-2 rotate-90 cursor-pointer"
+        className={`absolute right-3 p-2 cursor-pointer ${isOpen ? "rotate-180" : ""} transition-all duration-200`}
         onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
       >
-        {/* {isOpen? '▲' : '▼'} */}
-        {isOpen ? '<' : '>'}
+        ▼
       </button>
       <div
         className={`absolute right-7 bg-white top-8 flex flex-col justify-center shadow-md rounded-md
-          overflow-hidden transition-all duration-200 ease-in-out ${isOpen ? "max-h-36 z-10" : "max-h-0 z-10"}`}
+          overflow-hidden transition-all duration-200 ease-in-out ${isOpen ? "max-h-48 z-10" : "max-h-0 z-10"}`}
       >
         <button className="bg-white hover:bg-gray-100 px-4 py-2 text-left cursor-pointer">
           Редактировать
