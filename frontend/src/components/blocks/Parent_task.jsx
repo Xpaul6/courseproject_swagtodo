a4ad6e1e762a81c52e7492b55359858bca6be8cc
Pavel Kholkin <xpaul6@gmail.com>
2025-06-01 20:02:28 +0500

    fix: parent_task block code format

--- frontend/src/components/blocks/Parent_task.jsx
+++ frontend/src/components/blocks/Parent_task.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
 
 function Parent_task() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, SetIsOpen] = useState(false)
 
   return (
     <div className="flex justify-around items-center my-2 relative">
-      <div className={`w-fit mx-10 ${isOpen ? "underline" : ""}`}>Task description</div>
+      <div className={`w-fit mx-10 ${isOpen ? "underline" : ""}`}>
+        Task description
+      </div>
       <button
-        className={`absolute right-3 p-2 cursor-pointer ${isOpen ? "rotate-180" : ""} transition-all duration-200`}
-        onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
+        className={`absolute right-3 p-2 cursor-pointer ${
+          isOpen ? "rotate-180" : ""
+        } transition-all duration-200`}
+        onClick={() => (isOpen ? SetIsOpen(false) : SetIsOpen(true))}
       >
         ▼
       </button>
       <div
         className={`absolute right-7 bg-white top-8 flex flex-col justify-center shadow-md rounded-md
-          overflow-hidden transition-all duration-200 ease-in-out ${isOpen ? "max-h-48 z-10" : "max-h-0 z-10"}`}
+          overflow-hidden transition-all duration-200 ease-in-out z-10 ${
+            isOpen ? "max-h-36" : "max-h-0"
+          }`}
       >
         <button className="bg-white hover:bg-gray-100 px-4 py-2 text-left cursor-pointer">
           Редактировать
@@ -27,7 +33,7 @@ function Parent_task() {
         </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Parent_task
