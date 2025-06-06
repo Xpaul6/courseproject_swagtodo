6c3a291a435cb7b2ff5030b09237024feaa8f6e6
Pavel Kholkin <xpaul6@gmail.com>
2025-06-06 17:57:25 +0500

    fix: role var naming fix

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom'
 
 export function PrivateRoute({ children, roles }) {
   const isAuthenticated = localStorage.getItem('token')
-  const userRole = localStorage.getItem('userRole')
+  const userRole = localStorage.getItem('role')
 
   if (!isAuthenticated) {
     return <Navigate to="/register" replace />
